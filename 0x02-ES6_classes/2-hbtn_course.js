c260666f681d11eba7d1dd336f2462b1998593ca
enigmatik100 <m.p.gangbadja@gmail.com>
2023-01-06 00:48:08 +0100

    1. Let's make some classrooms

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,54 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+
+    if (students instanceof Array) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be a array');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(newName) {
+    if (!(typeof newName === 'string')) {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  set length(newLength) {
+    if (!(typeof newLength === 'number')) {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  set students(arraryStudent) {
+    if (!(arraryStudent instanceof Array)) {
+      throw new TypeError('Students must be a array');
+    }
+    this.students = arraryStudent;
+  }
+}
