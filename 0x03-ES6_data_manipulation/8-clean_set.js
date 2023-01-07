cbdfad8563377137f4a1e7850f1c83797fc158fa
enigmatik100 <m.p.gangbadja@gmail.com>
2023-01-07 15:08:21 +0100

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,9 +2,11 @@ const cleanSet = (set, startString) => {
   if (set instanceof Set && typeof startString === 'string' && startString.length > 0) {
     const res = [];
     for (const el of set) {
-      if (el.startsWith(startString)) {
-        res.push(el.substring(startString.length));
+      let substr = el;
+      while (substr.startsWith(startString)) {
+        substr = substr.substring(startString.length);
       }
+      res.push(substr);
     }
     return res.join('-');
   }
